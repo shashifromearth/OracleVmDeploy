7a4623c3ad35e65d1546adf998ee52ca0681666f
shashilpu <shashilpu@gmail.com>
2025-11-01 14:22:14 +0530

    times intervalsss

--- Deploy.sh
+++ Deploy.sh
@@ -2,20 +2,21 @@
 
 echo "🔧 Setting up one-click Ubuntu VM on Oracle Cloud Free Tier..."
 
-timeinSec=360  # in seconds
+timeinSec=3600  # 1 hour in seconds
 
 while true; do
-  last_run=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "🕒 Last attempt started at: $last_run"
+  last_run=$(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S")
+  echo "🕒 Last attempt started at: $last_run IST"
 
   terraform init
   terraform apply -auto-approve
 
   if [ $? -eq 0 ]; then
-    echo "✅ Deployment complete at $(date +"%Y-%m-%d %H:%M:%S"). Check your public IP above to connect via SSH or RDP."
+    success_time=$(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S")
+    echo "✅ Deployment complete at $success_time IST. Check your public IP above to connect via SSH or RDP."
     break
   fi
 
-  echo "⚠️ Capacity error. Retrying in $timeinSec seconds ..."
+  echo "⚠️ Capacity error. Retrying in $timeinSec seconds...🕒 Last attempt started at: $last_run IST"
   sleep $timeinSec
 done
