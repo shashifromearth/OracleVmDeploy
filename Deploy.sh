6a32cf77052b5c43554a805ebccf3f7a62afa959
shashilpu <shashilpu@gmail.com>
2025-11-01 20:23:10 +0530

    times intervalsss

--- Deploy.sh
+++ Deploy.sh
@@ -2,7 +2,7 @@
 
 echo "🔧 Setting up one-click Ubuntu VM on Oracle Cloud Free Tier..."
 
-timeinSec=3600  # 1 hour in seconds
+timeinSec=3400  # 1 hour in seconds
 
 while true; do
   last_run=$(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S")
