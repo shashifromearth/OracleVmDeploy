2ae24a67947a9d715c983d36831582fb3103ff1b
shashilpu <shashilpu@gmail.com>
2025-11-01 13:54:40 +0530

    times intervalss

--- Deploy.sh
+++ Deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
 echo "🔧 Setting up one-click Ubuntu VM on Oracle Cloud Free Tier..."
+
+timeinSec=360  # 1 hour in seconds
+
 while true; do
-terraform init
-terraform apply -auto-approve
-echo "Retrying in 1 hour due to capacity error..."
-  sleep 360
+  terraform init
+  terraform apply -auto-approve
+
+  if [ $? -eq 0 ]; then
+    echo "✅ Deployment complete. Check your public IP above to connect via SSH or RDP."
+    break
+  fi
+
+  echo "⚠️ Capacity error. Retrying in $timeinSec seconds..."
+  sleep $timeinSec
 done
-echo "✅ Deployment complete. Check your public IP above to connect via SSH or RDP."
